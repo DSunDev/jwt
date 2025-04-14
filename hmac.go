1bc4e3876a6f7a8c328f993115aef90bed2bde54
Dmitry Sunozov <d.sunozov.dev@ya.ru>
2025-04-14 18:22:25 +0700

    signers

---
+++ hmac.go
@@ -0,0 +1,27 @@
+package jwt
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+)
+
+type hs256 struct {
+	secretKey string
+}
+
+func NewHS256(secretKey string) hs256 {
+	h := hs256{secretKey: secretKey}
+	return h
+}
+
+func (h *hs256) Alg() string {
+	return "HS256"
+}
+
+func (h *hs256) Sign(headerB64 string, payloadB64 string) string {
+	hash := hmac.New(sha256.New, []byte(h.secretKey))
+	hash.Write([]byte(fmt.Sprintf("%s.%s", headerB64, payloadB64)))
+	return base64.RawURLEncoding.EncodeToString(hash.Sum(nil))
+}
--- jwt.go
+++ jwt.go
@@ -1,7 +1,74 @@
 package jwt
 
-import "fmt"
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strings"
+)
 
-func SayHelloFromJWT() {
-	fmt.Println("Hello!")
+type token struct {
+	header  map[string]string
+	payload map[string]string
+	signer  signer
+}
+
+type signer interface {
+	Alg() string
+	Sign(headerB64 string, payloadB64 string) string
+}
+
+func New(s signer) token {
+	t := token{
+		header:  make(map[string]string),
+		payload: make(map[string]string),
+		signer:  s,
+	}
+	t.header["typ"] = "JWT"
+	t.header["alg"] = s.Alg()
+	return t
+}
+
+func Verify(tokenB64 string, s signer) (bool, error) {
+	split := strings.Split(tokenB64, ".")
+	if len(split) != 3 {
+		return false, fmt.Errorf("invalid token structure")
+	}
+	validSign := s.Sign(split[0], split[1])
+	return split[2] == validSign, nil
+}
+
+func toB64String(data map[string]string) (string, error) {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		return "", nil
+	}
+	return base64.RawURLEncoding.EncodeToString(bytes), nil
+}
+
+func (t *token) Generete() (string, error) {
+	var err error
+	headerB64, err := toB64String(t.header)
+	if err != nil {
+		return "", err
+	}
+	payloadB64, err := toB64String(t.payload)
+	if err != nil {
+		return "", err
+	}
+	signatureB64 := t.signer.Sign(headerB64, payloadB64)
+	return fmt.Sprintf("%s.%s.%s", headerB64, payloadB64, signatureB64), nil
+}
+
+func (t *token) SetClaim(key string, value string) {
+	t.payload[key] = value
+}
+
+func (t *token) Claim(key string) (string, bool) {
+	claim, ok := t.payload[key]
+	if !ok {
+		return "", false
+	}
+
+	return claim, true
 }
