c62e5cc527fb8a99f52092fb92abba055aae4fef
Dmitry Sunozov <d.sunozov.dev@ya.ru>
2025-04-14 23:12:30 +0700

    improvements

--- hmac.go
+++ hmac.go
@@ -16,11 +16,11 @@ func NewHS256(secretKey string) hs256 {
 	return h
 }
 
-func (h *hs256) Alg() string {
+func (h hs256) Alg() string {
 	return "HS256"
 }
 
-func (h *hs256) Sign(headerB64 string, payloadB64 string) string {
+func (h hs256) Sign(headerB64 string, payloadB64 string) string {
 	hash := hmac.New(sha256.New, []byte(h.secretKey))
 	hash.Write([]byte(fmt.Sprintf("%s.%s", headerB64, payloadB64)))
 	return base64.RawURLEncoding.EncodeToString(hash.Sum(nil))
--- jwt.go
+++ jwt.go
@@ -7,68 +7,47 @@ import (
 	"strings"
 )
 
-type token struct {
-	header  map[string]string
-	payload map[string]string
-	signer  signer
-}
+type claims map[string]string
 
 type signer interface {
 	Alg() string
 	Sign(headerB64 string, payloadB64 string) string
 }
 
-func New(s signer) token {
-	t := token{
-		header:  make(map[string]string),
-		payload: make(map[string]string),
-		signer:  s,
-	}
-	t.header["typ"] = "JWT"
-	t.header["alg"] = s.Alg()
-	return t
+func NewClaims() claims {
+	return make(claims)
 }
 
-func Verify(tokenB64 string, s signer) (bool, error) {
+func Generate(claims claims, signer signer) (string, error) {
+	header := make(map[string]string)
+	header["typ"] = "JWT"
+	header["alg"] = signer.Alg()
+	headerB64, err := mapToB64Json(header)
+	if err != nil {
+		return "", err
+	}
+
+	payloadB64, err := mapToB64Json(map[string]string(claims))
+	if err != nil {
+		return "", err
+	}
+
+	signatureB64 := signer.Sign(headerB64, payloadB64)
+	return fmt.Sprintf("%s.%s.%s", headerB64, payloadB64, signatureB64), nil
+}
+
+func Verify(tokenB64 string, signer signer) (bool, error) {
 	split := strings.Split(tokenB64, ".")
 	if len(split) != 3 {
 		return false, fmt.Errorf("invalid token structure")
 	}
-	validSign := s.Sign(split[0], split[1])
-	return split[2] == validSign, nil
+	return split[2] == signer.Sign(split[0], split[1]), nil
 }
 
-func toB64String(data map[string]string) (string, error) {
+func mapToB64Json(data map[string]string) (string, error) {
 	bytes, err := json.Marshal(data)
 	if err != nil {
 		return "", nil
 	}
 	return base64.RawURLEncoding.EncodeToString(bytes), nil
-}
-
-func (t *token) Generete() (string, error) {
-	var err error
-	headerB64, err := toB64String(t.header)
-	if err != nil {
-		return "", err
-	}
-	payloadB64, err := toB64String(t.payload)
-	if err != nil {
-		return "", err
-	}
-	signatureB64 := t.signer.Sign(headerB64, payloadB64)
-	return fmt.Sprintf("%s.%s.%s", headerB64, payloadB64, signatureB64), nil
-}
-
-func (t *token) SetClaim(key string, value string) {
-	t.payload[key] = value
-}
-
-func (t *token) Claim(key string) (string, bool) {
-	claim, ok := t.payload[key]
-	if !ok {
-		return "", false
-	}
-
-	return claim, true
 }
